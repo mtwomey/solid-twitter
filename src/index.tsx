8c653f4473ad65e1d16d899f5b12e42509a2df35
Matthew Twomey <mtwomey@Matthews-MacBook-Air.local>
2022-06-05 00:54:21 -0500

    Add code exchange

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,54 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
 import { createSignal } from 'solid-js';
-
-const authUri = 'https://twitter.com/i/oauth2/authorize';
-const params = {
-  response_type: 'code',
-  client_id: 'ZGFZeGV6MmRZNkdBNXY0MVdGeW06MTpjaQ',
-  redirect_uri: 'https://solid-twitter.herokuapp.com/callback/',
-  scope: 'tweet.read',
-  state: 'state',
-  code_challenge: 'challenge',
-  code_challenge_method: 'plain'
-}
-const fullUrl = `${authUri}?${Object.keys(params).map(key => `${key}=${params[key]}`).join('&')}`;
+import qs from 'qs';
+import axios from 'axios';
 
 function authInit() {
-  console.log(fullUrl);
+  const authUri = 'https://twitter.com/i/oauth2/authorize';
+  const params = qs.stringify({
+    response_type: 'code',
+    client_id: 'ZGFZeGV6MmRZNkdBNXY0MVdGeW06MTpjaQ',
+    redirect_uri: `${window.location.href}callback/`,
+    scope: 'tweet.read',
+    state: 'state',
+    code_challenge: 'challenge',
+    code_challenge_method: 'plain'
+  });
+  const fullUrl = `${authUri}?${params}`;
+
   window.open(fullUrl, 'authWindow', 'height=700,width=500');
 }
 
 const [twitterCode, setTwitterCode] = createSignal('Unknown');
 
-window.addEventListener("storage", () => {
-  console.log('something happened');
-  setTwitterCode(localStorage.getItem('twitterAuthCode'));
-}, false);
+function handleLocalStorageEvent(e) {
+  if (Object.keys(e.storageArea).includes('twitterAuthCode')) {
+    setTwitterCode(localStorage.getItem('twitterAuthCode'));
+
+    axios.post('https://api.twitter.com/2/oauth2/token', {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      data: qs.stringify({
+        code: twitterCode(),
+        grant_type: 'authorization_code',
+        client_id: 'ZGFZeGV6MmRZNkdBNXY0MVdGeW06MTpjaQ',
+        redirect_uri: 'http://127.0.0.1:3000/callback/',
+        code_verifier: 'challenge'
+      })
+    }).then(result => {
+      let x = result;
+      let y = x;
+    }).catch(error => {
+      console.log(error);
+    });
+
+    // window.removeEventListener("storage", handleLocalStorageEvent, false); // If we want to remove it
+  }
+}
+
+window.addEventListener("storage", handleLocalStorageEvent, false);
 
 function Auth () {
   return (
