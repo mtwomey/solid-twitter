fd809530ee2314e34b489d0ba55c7e6bbaeb2967
Matthew Twomey <mtwomey@Matthews-MacBook-Air.local>
2022-06-06 22:06:25 -0500

    Fix asynchronous signal

--- src/index.tsx
+++ src/index.tsx
@@ -21,24 +21,32 @@ function authInit() {
   window.open(fullUrl, 'authWindow', 'height=700,width=500');
 }
 
-const [twitterCode, setTwitterCode] = createSignal('Unknown');
+const [twitterAuthCode, setTwitterAuthCode] = createSignal('Unknown');
 const [twitterAuthResult, setTwitterAuthResult] = createSignal('Unknown');
 
 function handleLocalStorageEvent(e) {
   if (Object.keys(e.storageArea).includes('twitterAuthCode')) {
-    setTwitterCode(localStorage.getItem('twitterAuthCode'));
+    const twitterAuthCode = localStorage.getItem('twitterAuthCode');
+    setTwitterAuthCode(twitterAuthCode); // This doesn't necessairly happen right away....
 
-    axios.post('https://api.pearpop-dev.com/v1/socialproxy/twitter/userInfo', {
-      code: twitterCode(),
-      clientId: 'ZGFZeGV6MmRZNkdBNXY0MVdGeW06MTpjaQ',
-      redirectUri: `${window.location.href}callback/`
-    }, {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    }).then(response => {
-      setTwitterAuthResult(JSON.stringify(response.data, null, 2));
-    })
+    const target = 'https://api.pearpop-dev.com/v1/socialproxy/twitter/userInfo';
+    // const target = 'http://localhost:4000/socialproxy/twitter/userInfo';
+    try {
+      axios.post(target, {
+        code: twitterAuthCode,
+        clientId: 'ZGFZeGV6MmRZNkdBNXY0MVdGeW06MTpjaQ',
+        redirectUri: `${window.location.href}callback/`
+      }, {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      }).then(response => {
+        setTwitterAuthResult(JSON.stringify(response.data, null, 2));
+      })
+    } catch (e) {
+      let x = e;
+    }
+    debugger
   }
 }
 
@@ -50,7 +58,7 @@ function Auth() {
       <h2>Authorize Twitter</h2>
       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded border-0" onClick={authInit}> Authorize </button>
       <h2>Auth Code</h2>
-      <h4 class="text-red-400 text-lg">{twitterCode}</h4>
+      <h4 class="text-red-400 text-lg">{twitterAuthCode}</h4>
       <h2>Result</h2>
       <pre class="text-green-600 text-lg">{twitterAuthResult}</pre>
     </div>
